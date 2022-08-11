fc67a362c11f52226811dc6651aa3e143d53d8dd
Denis <den_makarcev@mail.ru>
2022-08-11 20:31:11 +0300

    Все для Вас 5.0!!!

--- Bots.py
+++ Bots.py
@@ -6,60 +6,75 @@ bot = Bot(token='5455540322:AAFSyeSpqCKuiGvskoyJVE-AR8jCb-QP4po')
 updater = Updater(token='5455540322:AAFSyeSpqCKuiGvskoyJVE-AR8jCb-QP4po')
 dispatcher = updater.dispatcher
 
-STATE0 = 1
-STATE1 = 2
-STATE2 = 3
+START = 0
+NUMBERFIRST = 1
+NUMBERSECOND = 2
+OPERATION = 3
+numberOne = ''
+numberTwo = ''
+oper = ''
+
+
+def numbers(number):
+    pass
+
+
+def result(x, y, z):
+    pass
 
 
 def start(update, context):
-    context.bot.send_message(update.effective_chat.id, 'Напиши мне "Привет" и мы начнем болтать')
+    context.bot.send_message(update.effective_chat.id, 'Добро пожаловать в бота, который умеет '
+                                                       'считать комплексные и рациональные числа! Напиши 2 числа\n'
+                                                       'Введи первое число: ')
 
-    return STATE0
-
-
-def message0(update, context):
-    if 'прив' in update.message.text.lower():
-        context.bot.send_message(update.effective_chat.id, 'Привет!\nКак твои дела?')
-        return STATE1
-    else:
-        context.bot.send_message(update.effective_chat.id, 'Я тебя не очень хорошо понял, напиши мне "Привет".')
+    return NUMBERFIRST
 
 
-def message1(update, context):
-    if 'хор' in update.message.text.lower() or 'отл' in update.message.text.lower():
-        context.bot.send_message(update.effective_chat.id, 'Отлично, я очень рад!')
-    elif 'плох' in update.message.text.lower():
-        context.bot.send_message(update.effective_chat.id, 'Не переживай все наладится, я в тебя верю!')
+def numberFirst(update, context):
+    global numberOne
+    numberOne = numbers(update.message.text)
+    context.bot.send_message(update.effective_chat.id, 'Отлично!\nВведи второе число: ')
 
-    context.bot.send_message(update.effective_chat.id, 'Чем сегодня занимаешься?')
-    return STATE2
+    return NUMBERSECOND
 
 
-def message2(update, context):
-    context.bot.send_message(update.effective_chat.id, 'Мне без разницы, спросил, чтобы протестировать бота!')
+def numberSecond(update, context):
+    global numberTwo
+    numberTwo = numbers(update.message.text)
+    context.bot.send_message(update.effective_chat.id, 'Отлично!\nВведи операцию (+, -, *, /): ')
+
+    return OPERATION
+
+
+def operation(update, context):
+    global oper
+
+    context.bot.send_message(update.effective_chat.id, f'Результат: {result(numberOne, numberTwo, update.message.text)}')
+
     return ConversationHandler.END
 
 
 def cancel(update, context):
-    context.bot.send_message(update.effective_chat.id, 'Рад был пообщаться! Всего доброго!')
+    context.bot.send_message(update.effective_chat.id, 'Прощай!')
 
     return ConversationHandler.END
 
 
 start_handler = CommandHandler('start', start)
 cancel_handler = CommandHandler('cancel', cancel)
-message0_handler = MessageHandler(Filters.text, message0)
-message1_handler = MessageHandler(Filters.text, message1)
-message2_handler = MessageHandler(Filters.text, message2)
-conv_handler = ConversationHandler(entry_points=[start_handler],
-                                   states={STATE0: [message0_handler],
-                                           STATE1: [message1_handler],
-                                           STATE2: [message2_handler]},
-                                   fallbacks=[cancel_handler])
+numone_handler = MessageHandler(Filters.text, numberFirst)
+numtwo_handler = MessageHandler(Filters.text, numberSecond)
+oper_handler = MessageHandler(Filters.text, operation)
+conv_handler = ConversationHandler(fallbacks=[start_handler],
+                                   states={
+                                       NUMBERFIRST: [numone_handler],
+                                       NUMBERSECOND: [numtwo_handler],
+                                       OPERATION: [oper_handler]
+                                   },
+                                   entry_points=[cancel_handler])
 
-dispatcher.add_handler(cancel_handler)
-dispatcher.add_handler(conv_handler)
 
-# В конце |||
+
 updater.start_polling()
-updater.idle()  # ctrl + C
+updater.idle()
